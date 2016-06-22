31e4f8206ed6432fed0acdb59a3eae363e7643ce
kb-ma <kbma.kevin@gmail.com>
2016-06-21 21:41:23 -0400

    added demo for application state, session state, cookies

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,10 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : Page
+public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        HttpCookie userCookie;
+        userCookie = Request.Cookies["UserID"];
+        if (userCookie == null)
+        {
+            myLabel.Text = "Cookie doesn't exist! Creating a cookie now.";
+            userCookie = new HttpCookie("UserID", "Joe Black");
+            userCookie.Expires = DateTime.Now.AddMonths(1);
+            Response.Cookies.Add(userCookie);
+        }
+        else
+        {
+            myLabel.Text = "Welcome back, " + userCookie.Value;
+        }
     }
 }
